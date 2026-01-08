1302077fa0b12660016a6aa5f8d871f7089af2fb
melroser <themelroser@gmail.com>
2026-01-08 02:07:08 -0500

    closly matching the setup for the webapp for posthog

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -9,8 +9,14 @@ const key = process.env.NEXT_PUBLIC_POSTHOG_KEY;
 if (typeof window !== "undefined" && key) {
   posthog.init(key, {
     api_host: apiHost,
+    session_recording: {
+        maskAllInputs: true,
+    },
+    mask_all_text: false,
+    cross_subdomain_cookie: true,
     ui_host: uiHost,
     defaults: "2025-11-30",
+    capture_pageview: true,
     capture_exceptions: true,
     debug: !isProd,
     person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
